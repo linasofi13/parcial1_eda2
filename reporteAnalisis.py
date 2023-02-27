766b820d5cb8cfd2cff22928d682bd252bc901c7
Lina <103126242+linasofi13@users.noreply.github.com>
2023-02-26 19:05:15 -0500

    Update reporteAnalisis.py

--- reporteAnalisis.py
+++ reporteAnalisis.py
@@ -1,4 +1,4 @@
-from triajeClasificacion import *
+from triageClasificacion import *
 import random
 
 def convertirAFormato(segundos):
@@ -36,19 +36,20 @@ def tiempoPromedioTratamiento():
     promedio = (1800*promedio) + 600
     return promedio
     
-def tiempoTotalPorPaciente(paciente, triaje, listaPacientes):
-    tiempoIngresoATriaje = random.randint(60, 120) # (s) del ingreso del paciente a urgencias al triaje de clasificacion esta entre 1 a 2 minutos
-    tiempoTriajeAPrediagnostico = 0
-    if triaje == 1:
-        tiempoTriajeAPrediagnostico = 0 # atencion inmediata
-    if triaje == 2:
-        tiempoTriajeAPrediagnostico = random.randint(600, 900) # 10-15 min (en segundos)
-    if triaje == 3:
-        tiempoTriajeAPrediagnostico = random.randint(1800, 3600) #30-60 min
-    if triaje == 4:
-        tiempoTriajeAPrediagnostico = random.randint(3600, 7200) #60-120 min}
+def tiempoTotalPorPaciente(paciente, triage, listaPacientes):
+    tiempoIngresoATriage = random.randint(60, 120) # (s) del ingreso del paciente a urgencias al triage de clasificacion esta entre 1 a 2 minutos
+    # tiempoTriageAPrediagnostico = 0
+    
+    if ((triage == 1) or ((paciente.condicionPrioritaria == "Discapacitado") or (paciente.condicionPrioritaria == "Embarazada") or (paciente.condicionPrioritaria == "Adulto Mayor"))) :
+        tiempoTriageAPrediagnostico = 0 # atencion inmediata
+    elif triage == 2:
+        tiempoTriageAPrediagnostico = random.randint(600, 900) # 10-15 min (en segundos)
+    elif triage == 3:
+        tiempoTriageAPrediagnostico = random.randint(1800, 3600) #30-60 min
+    elif triage == 4:
+        tiempoTriageAPrediagnostico = random.randint(3600, 7200) #60-120 min}
     else:
-        tiempoTriajeAPrediagnostico = random.randint(7200, 14400) # 2 a 4 horas
+        tiempoTriageAPrediagnostico = random.randint(7200, 14400) # 2 a 4 horas
         
     tiempoPrediagnostico = tiemposPred[listaPacientes.index(paciente)] # 10 a 20 min
     tiempoLaboratorio = tiemposLab[listaPacientes.index(paciente)] # 10 a 30 min
@@ -58,8 +59,5 @@ def tiempoTotalPorPaciente(paciente, triaje, listaPacientes):
     segundosLab = (1200*tiempoLaboratorio) + 600
     segundosTratamiento = (1800*tiempoTratamiento) + 600
     
-    tiempoTotal = tiempoIngresoATriaje + tiempoTriajeAPrediagnostico + segundosPred + segundosLab + segundosTratamiento
-    return tiempoTotal
-
-
-    
+    tiempoTotal = tiempoIngresoATriage + tiempoTriageAPrediagnostico + segundosPred + segundosLab + segundosTratamiento
+    return tiempoIngresoATriage, tiempoTriageAPrediagnostico, tiempoTotal
